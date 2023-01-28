1ec700262f042631b184fdfef44b1be2810f0357
Andrii Tsokur <tsokur@gmail.com>
2023-01-28 03:13:42 +0200

    Removes ugly animation

--- script/filter.js
+++ script/filter.js
@@ -9,25 +9,13 @@
 			if (radio.value === 'all') {
 				portfolioItems.forEach(item => {
 					item.classList.remove('portfolio-item-is-hidden');
-
-					setTimeout(function () {
-						item.classList.remove('portfolio-item-transform');
-					}, 250);
 				});
 			} else {
 				portfolioItems.forEach(item => {
 					if (item.dataset.filter === radio.value) {
 						item.classList.remove('portfolio-item-is-hidden');
-
-						setTimeout(function () {
-							item.classList.remove('portfolio-item-transform');
-						}, 250);
 					} else {
-						item.classList.add('portfolio-item-transform');
-
-						setTimeout(function () {
-							item.classList.add('portfolio-item-is-hidden');
-						}, 250);
+						item.classList.add('portfolio-item-is-hidden');
 					}
 				});
 			}
